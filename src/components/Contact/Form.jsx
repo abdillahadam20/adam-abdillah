f1c25c579000322c7122da27ac6c1036002a9ef9
abdillahadam20 <adamabdillah20@gmail.com>
2022-05-21 01:17:40 +0700

    fixing email form

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import emailjs from '@emailjs/browser';
-import Swal from 'sweetalert2'
+import { useRef } from 'react';
+import Swal from 'sweetalert2';
 
-const SERVICE_ID = "service_6ttcnbe";
+const SERVICE_ID = "service_92vayo9";
 const TEMPLATE_ID = "template_t6yd9ks";
-const USER_ID = "97SJ-Z2WpGfqMeWJ4";
+const PUBLIC_KEY = "97SJ-Z2WpGfqMeWJ4";
 
-export default function Form() {
+const Form = () => {
+  // const form = useRef(null);
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
       .then((result) => {
         console.log(result.text);
         Swal.fire({
@@ -24,20 +28,20 @@ export default function Form() {
           text: error.text,
         })
       });
-    e.target.reset()
-  };
+    e.target.reset();
+  }
 
-  return (
+  return(
     <>
-      <Form onSubmit={handleOnSubmit}>
+      <form onSubmit={handleOnSubmit}>
         <div className="w-full lg:w-2/3 lg:mx-auto">
           <div className="w-full px-4 mb-8">
             <label htmlFor="name" className="text-base text-primary font-bold">Nama</label>
-            <input type="text" id="name" placeholder="Type here" className="w-full bg-white outline outline-gray-300 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary" />
+            <input type="name" name="name" id="name" placeholder="Type here" className="w-full bg-white outline outline-gray-300 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary" />
           </div>
           <div className="w-full px-4 mb-8">
             <label htmlFor="email" className="text-base text-primary font-bold">Email</label>
-            <input type="email" id="email" placeholder="Type here" className="w-full bg-white outline outline-gray-300 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary" />
+            <input type="email" name="email" id="email" placeholder="Type here" className="w-full bg-white outline outline-gray-300 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary" />
           </div>
           <div className="w-full px-4 mb-8">
             <label htmlFor="name" className="text-base text-primary font-bold">Pesan</label>
@@ -47,7 +51,9 @@ export default function Form() {
             <button type="submit" className="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-500">Kirim</button>
           </div>
         </div>
-      </Form>
+      </form>
     </>
   )
 }
+
+export default Form;
